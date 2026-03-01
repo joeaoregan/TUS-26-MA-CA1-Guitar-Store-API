9f944597c8fa9c088b40f7a1d4f5bc0f8a065680
joeaoregan <joe.oregan@gmail.com>
2026-03-01 01:35:49 +0000

    Initial commit: Spring Boot project structure for Guitar Store API

---
+++ src/main/java/edu/tus/guitarstore/Tus26MaCa1GuitarStoreApiApplication.java
@@ -0,0 +1,13 @@
+package edu.tus.guitarstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tus26MaCa1GuitarStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tus26MaCa1GuitarStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tus/guitarstore/Tus26MaCa1GuitarStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tus.guitarstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tus26MaCa1GuitarStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
