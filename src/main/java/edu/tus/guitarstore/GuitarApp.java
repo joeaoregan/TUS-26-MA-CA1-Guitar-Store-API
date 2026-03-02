e9320591d10a37d5e1f4ce4f5fabad19b2a44042
joeaoregan <joe.oregan@gmail.com>
2026-03-02 13:25:38 +0000

    fix main

--- src/main/java/edu/tus/guitarstore/GuitarApp.java
+++ src/main/java/edu/tus/guitarstore/GuitarApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GuitarApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(GuitarManagerApp.class, args);
+        SpringApplication.run(GuitarApp.class, args);
     }
 
 }
