f412fa8e3390a943d1336f6c94a6fba9eb320b71
joeaoregan <joe.oregan@gmail.com>
2026-03-04 16:42:32 +0000

    Audit: helper functon for createdBy on new guitar model

---
+++ src/main/java/edu/tus/guitarstore/audit/AuditAwareImpl.java
@@ -0,0 +1,18 @@
+/*
+ * Helper Class. 
+ * Ensures the created_by and updated_by fields show a specific value instead of being empty.
+ */
+
+package edu.tus.guitarstore.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Joe");
+    }
+}
