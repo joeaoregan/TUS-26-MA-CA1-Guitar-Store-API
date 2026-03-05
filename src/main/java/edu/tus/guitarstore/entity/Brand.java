e1733af190180ab11d93f58e6c2539ac5d114c6c
joeaoregan <joe.oregan@gmail.com>
2026-03-05 00:57:06 +0000

    refactor Brand.java imports

--- src/main/java/edu/tus/guitarstore/entity/Brand.java
+++ src/main/java/edu/tus/guitarstore/entity/Brand.java
@@ -1,22 +1,28 @@
 package edu.tus.guitarstore.entity;
 
+import java.util.List;
+
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
-    private Long id;
-    
-    private String name;
-    
-    private String country;
-    
-    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
-    @ToString.Exclude // prevent infinite loops with Lombok
-    private java.util.List<Guitar> guitars;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	private String name;
+
+	private String country;
+
+	@OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
+	@ToString.Exclude // prevent infinite loops with Lombok
+	private List<Guitar> guitars;
 }
