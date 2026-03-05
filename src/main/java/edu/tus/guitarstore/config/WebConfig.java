e24c810bc527f1ad3bb3902fc77da93f96e601c6
joeaoregan <joe.oregan@gmail.com>
2026-03-05 14:55:56 +0000

    add static landing page

---
+++ src/main/java/edu/tus/guitarstore/config/WebConfig.java
@@ -0,0 +1,15 @@
+package edu.tus.guitarstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Explicitly map the root URL to the static index.html
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+}
