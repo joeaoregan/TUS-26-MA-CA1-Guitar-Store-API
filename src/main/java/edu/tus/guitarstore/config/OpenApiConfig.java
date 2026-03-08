2caa26fce7dad9e48ece2737a5ab76e0ccfb2c0a
joeaoregan <joe.oregan@gmail.com>
2026-03-08 20:10:23 +0000

    OpenApiConfig additional info

--- src/main/java/edu/tus/guitarstore/config/OpenApiConfig.java
+++ src/main/java/edu/tus/guitarstore/config/OpenApiConfig.java
@@ -1,9 +1,13 @@
 package edu.tus.guitarstore.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
+import java.util.Map;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,11 +16,16 @@ public class OpenApiConfig {
 
 	@Bean
 	public OpenAPI guitarStoreOpenAPI() {
-		return new OpenAPI().info(new Info().title("Guitar Store Management API")
-				.description("A comprehensive RESTful API for managing musical instrument inventory. "
-						+ "Developed as part of the Microservices Architecture Module. "
-						+ "Key features include N-tier architecture, JPA Auditing, and DTO-based separation.")
-				.version("v1.0.0").contact(new Contact().name("Joe O'Regan").email("A00258304@student.tus.ie"))
-				.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")));
+		return new OpenAPI()
+				.info(new Info().title("Guitar Store Management API")
+						.description("A comprehensive RESTful API for managing musical instrument inventory. "
+								+ "Developed as part of the Microservices Architecture Module. "
+								+ "Key features include N-tier architecture, JPA Auditing, and DTO-based separation.")
+						.version("v1.0.0").contact(new Contact().name("Joe O'Regan").email("A00258304@student.tus.ie"))
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
+				.extensions(Map.of("x-keywords", "Spring Boot, REST, Microservices, JPA, Guitar Store"))
+				.externalDocs(new ExternalDocumentation().description("Guitar Store API Source Code & Project Wiki")
+						.url("https://github.com/joeaoregan/TUS-26-MA-CA1-Guitar-Store-API"));
 	}
 }
