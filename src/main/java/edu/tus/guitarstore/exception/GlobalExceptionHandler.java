fc5bce9528b9daf986f5f92c05717cfa56c80224
joeaoregan <joe.oregan@gmail.com>
2026-03-08 19:49:27 +0000

    Add BrandAlsreadExistsException

--- src/main/java/edu/tus/guitarstore/exception/GlobalExceptionHandler.java
+++ src/main/java/edu/tus/guitarstore/exception/GlobalExceptionHandler.java
@@ -22,82 +22,94 @@ import edu.tus.guitarstore.dto.ErrorResponseDto;
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(GuitarAlreadyExistsException.class)
-	public ResponseEntity<ErrorResponseDto> handleGuitarAlreadyExistsException(GuitarAlreadyExistsException exception,
-			WebRequest webRequest) {
-		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
-				HttpStatus.BAD_REQUEST, exception.getMessage(), LocalDateTime.now());
-		return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(GuitarAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleGuitarAlreadyExistsException(GuitarAlreadyExistsException exception,
+            WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST, exception.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
 
-	/*
+    @ExceptionHandler(BrandAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleBrandAlreadyExistsException(BrandAlreadyExistsException exception,
+            WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    /*
 	 * 404 Not Found handling Handle ResourceNotFoundException
-	 */
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
-			WebRequest webRequest) {
-		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false), HttpStatus.NOT_FOUND, // 404
-				exception.getMessage(), LocalDateTime.now());
-		return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
-	}
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException exception,
+            WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false), HttpStatus.NOT_FOUND, // 404
+                exception.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
 
-	/*
+    /*
 	 * 404 Not Found handling Handle incorrect URL paths / routing errors
-	 */
-	@Override
-	protected ResponseEntity<Object> handleNoResourceFoundException(
-			org.springframework.web.servlet.resource.NoResourceFoundException ex, HttpHeaders headers,
-			HttpStatusCode status, WebRequest request) {
+     */
+    @Override
+    protected ResponseEntity<Object> handleNoResourceFoundException(
+            org.springframework.web.servlet.resource.NoResourceFoundException ex, HttpHeaders headers,
+            HttpStatusCode status, WebRequest request) {
 
-		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(request.getDescription(false), HttpStatus.NOT_FOUND,
-				"The requested URL path is invalid: " + ex.getMessage(), LocalDateTime.now());
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(request.getDescription(false), HttpStatus.NOT_FOUND,
+                "The requested URL path is invalid: " + ex.getMessage(), LocalDateTime.now());
 
-		return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
-	}
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
 
-	/*
+    /*
 	 * 500 Internal Server Error Catch-all exception handling
-	 */
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest) {
-		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
-				HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), LocalDateTime.now());
-		return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest) {
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	/*
+    /*
 	 * 400 Bad Request handling Handle MethodArgumentNotValidException
-	 */
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-		Map<String, String> validationErrors = new HashMap<>();
-		List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
 
-		validationErrorList.forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
-			String validationMsg = error.getDefaultMessage();
-			validationErrors.put(fieldName, validationMsg);
-		});
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
 
-		return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
-	}
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
 
-	/*
+    /*
 	 * 400 Bad Request handling Handle MethodArgumentTypeMismatchException
-	 */
-	@ExceptionHandler(org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class)
-	public ResponseEntity<ErrorResponseDto> handleTypeMismatchException(
-			org.springframework.web.method.annotation.MethodArgumentTypeMismatchException exception,
-			WebRequest webRequest) {
+     */
+    @ExceptionHandler(org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponseDto> handleTypeMismatchException(
+            org.springframework.web.method.annotation.MethodArgumentTypeMismatchException exception,
+            WebRequest webRequest) {
 
-		String message = String.format("The parameter '%s' should be of type '%s'", exception.getName(),
-				exception.getRequiredType().getSimpleName());
+        String message = String.format("The parameter '%s' should be of type '%s'", exception.getName(),
+                exception.getRequiredType().getSimpleName());
 
-		ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
-				HttpStatus.BAD_REQUEST, message, LocalDateTime.now());
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST, message, LocalDateTime.now());
 
-		return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
-	}
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
 }
