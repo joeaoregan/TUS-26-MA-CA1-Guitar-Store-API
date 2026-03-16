967c0e764155faf78ed976d5adc34aa8faf29b82
joeaoregan <joe.oregan@gmail.com>
2026-03-16 13:16:12 +0000

    application.yml add title and description

--- src/main/java/edu/tus/guitarstore/config/OpenApiConfig.java
+++ src/main/java/edu/tus/guitarstore/config/OpenApiConfig.java
@@ -16,27 +16,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-	
+
+    @Value("${app.title}")
+    private String appTitle;
+
 	@Value("${spring.application.version}")
-    private String appVersion;
-	
+	private String appVersion;
+
 	@Value("${app.author}")
-    private String appAuthor;
+	private String appAuthor;
 
-    @Value("${app.email}")
-    private String appEmail;
+	@Value("${app.email}")
+	private String appEmail;
 
-    @Value("${app.github}")
-    private String appGithub;
+	@Value("${app.github}")
+	private String appGithub;
+
+	@Value("${app.description}")
+	private String appDescription;
 
 	@Bean
 	OpenAPI guitarStoreOpenAPI() {
 		return new OpenAPI()
-				.info(new Info().title("Guitar Store Management API")
-						.description("A comprehensive RESTful API for managing musical instrument inventory. "
-								+ "Developed as part of the Microservices Architecture Module. "
-								+ "Key features include N-tier architecture, JPA Auditing, and DTO-based separation.")
-						.version(appVersion).contact(new Contact().name(appAuthor).email(appEmail))
+				.info(new Info().title(appTitle).description(appDescription).version(appVersion)
+						.contact(new Contact().name(appAuthor).email(appEmail))
 						.termsOfService("http://swagger.io/terms/")
 						.license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")))
 				.extensions(Map.of("x-keywords", "Spring Boot, REST, Microservices, JPA, Guitar Store"))
