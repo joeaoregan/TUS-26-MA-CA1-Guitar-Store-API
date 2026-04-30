6363bebca0ec86cacf286fd37e90dce5c29379a4
joeaoregan <joe.oregan@gmail.com>
2026-04-30 21:57:54 +0100

    sonar: fix non compliant code in BrandController - remove ApiResponses wrapper

--- src/main/java/edu/tus/guitarstore/controller/BrandController.java
+++ src/main/java/edu/tus/guitarstore/controller/BrandController.java
@@ -24,78 +24,75 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @Tag(name = "Brand Controller", description = "CRUD operations for Guitar Brands")
 @RestController
-@RequestMapping(path = "/api/guitarstore/v1/brands", produces = { MediaType.APPLICATION_JSON_VALUE })
+@RequestMapping(path = "/api/guitarstore/v1/brands", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 public class BrandController {
 
-	private IBrandService iBrandService;
+    private IBrandService iBrandService;
 
-	@Operation(summary = "Create Brand", description = "REST API to create a new Guitar Brand")
-	@ApiResponses({
-			@ApiResponse(responseCode = "201", description = "HTTP Status Created", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
-			@ApiResponse(responseCode = "400", description = "HTTP Status Bad Request", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
-	@PostMapping
-	public ResponseEntity<ResponseDto> createBrand(@Valid @RequestBody BrandDto brandDto) {
-		iBrandService.createBrand(brandDto);
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(new ResponseDto(GuitarStoreConstants.STATUS_201, "Brand created successfully"));
-	}
+    @Operation(summary = "Create Brand", description = "REST API to create a new Guitar Brand")
+    @ApiResponse(responseCode = "201", description = "HTTP Status Created", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    @ApiResponse(responseCode = "400", description = "HTTP Status Bad Request", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    @PostMapping
+    public ResponseEntity<ResponseDto> createBrand(@Valid @RequestBody BrandDto brandDto) {
+        iBrandService.createBrand(brandDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new ResponseDto(GuitarStoreConstants.STATUS_201, "Brand created successfully"));
+    }
 
-	@Operation(summary = "Fetch All Brands", description = "REST API to fetch all available Guitar Brands")
-	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK") })
-	@GetMapping
-	public ResponseEntity<List<BrandDto>> fetchAllBrands() {
-		List<BrandDto> allBrands = iBrandService.fetchAllBrands();
-		return ResponseEntity.status(HttpStatus.OK).body(allBrands);
-	}
+    @Operation(summary = "Fetch All Brands", description = "REST API to fetch all available Guitar Brands")
+    @ApiResponse(responseCode = "200", description = "HTTP Status OK")
+    @GetMapping
+    public ResponseEntity<List<BrandDto>> fetchAllBrands() {
+        List<BrandDto> allBrands = iBrandService.fetchAllBrands();
+        return ResponseEntity.status(HttpStatus.OK).body(allBrands);
+    }
 
-	@Operation(summary = "Fetch Brand Details", description = "REST API to fetch Brand details based on a unique brand name")
-	@ApiResponses({ @ApiResponse(responseCode = "200", description = "HTTP Status OK"),
-			@ApiResponse(responseCode = "404", description = "HTTP Status Not Found", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
-	@GetMapping("/{brandName}")
-	public ResponseEntity<BrandDto> fetchBrand(
-			@Parameter(description = "The unique name of the brand", example = "Gibson") @PathVariable String brandName) {
-		BrandDto brandDto = iBrandService.fetchBrand(brandName);
-		return ResponseEntity.status(HttpStatus.OK).body(brandDto);
-	}
+    @Operation(summary = "Fetch Brand Details", description = "REST API to fetch Brand details based on a unique brand name")
+    @ApiResponse(responseCode = "200", description = "HTTP Status OK")
+    @ApiResponse(responseCode = "404", description = "HTTP Status Not Found", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    @GetMapping("/{brandName}")
+    public ResponseEntity<BrandDto> fetchBrand(
+            @Parameter(description = "The unique name of the brand", example = "Gibson") @PathVariable String brandName) {
+        BrandDto brandDto = iBrandService.fetchBrand(brandName);
+        return ResponseEntity.status(HttpStatus.OK).body(brandDto);
+    }
 
-	@Operation(summary = "Update Brand", description = "REST API to update existing Brand details")
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "HTTP Status OK", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
-			@ApiResponse(responseCode = "417", description = "Expectation Failed", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
-			@ApiResponse(responseCode = "400", description = "Bad Request (Validation)", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))) })
-	@PutMapping
-	public ResponseEntity<ResponseDto> updateBrand(@Valid @RequestBody BrandDto brandDto) {
-		boolean isUpdated = iBrandService.updateBrand(brandDto);
-		if (isUpdated) {
-			return ResponseEntity.status(HttpStatus.OK)
-					.body(new ResponseDto(GuitarStoreConstants.STATUS_200, "Brand updated successfully"));
-		}
-		return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
-				.body(new ResponseDto(GuitarStoreConstants.STATUS_417, "Update failed"));
-	}
+    @Operation(summary = "Update Brand", description = "REST API to update existing Brand details")
+    @ApiResponse(responseCode = "200", description = "HTTP Status OK", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    @ApiResponse(responseCode = "417", description = "Expectation Failed", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    @ApiResponse(responseCode = "400", description = "Bad Request (Validation)", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    @PutMapping
+    public ResponseEntity<ResponseDto> updateBrand(@Valid @RequestBody BrandDto brandDto) {
+        boolean isUpdated = iBrandService.updateBrand(brandDto);
+        if (isUpdated) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(GuitarStoreConstants.STATUS_200, "Brand updated successfully"));
+        }
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(GuitarStoreConstants.STATUS_417, "Update failed"));
+    }
 
-	@Operation(summary = "Delete Brand", description = "REST API to delete a Brand and all its associated guitar inventory")
-	@ApiResponses({
-			@ApiResponse(responseCode = "200", description = "HTTP Status OK", content = @Content(schema = @Schema(implementation = ResponseDto.class))),
-			@ApiResponse(responseCode = "417", description = "Expectation Failed", content = @Content(schema = @Schema(implementation = ResponseDto.class))) })
-	@DeleteMapping("/{brandName}")
-	public ResponseEntity<ResponseDto> deleteBrand(
-			@Parameter(description = "The unique name of the brand to delete", example = "Ibanez") @PathVariable String brandName) {
-		boolean isDeleted = iBrandService.deleteBrand(brandName);
-		if (isDeleted) {
-			return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(GuitarStoreConstants.STATUS_200,
-					"Brand and associated guitars deleted successfully"));
-		} else {
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
-					.body(new ResponseDto(GuitarStoreConstants.STATUS_417, "Delete operation failed"));
-		}
-	}
+    @Operation(summary = "Delete Brand", description = "REST API to delete a Brand and all its associated guitar inventory")
+    @ApiResponse(responseCode = "200", description = "HTTP Status OK", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    @ApiResponse(responseCode = "417", description = "Expectation Failed", content = @Content(schema = @Schema(implementation = ResponseDto.class)))
+    @DeleteMapping("/{brandName}")
+
+    public ResponseEntity<ResponseDto> deleteBrand(
+            @Parameter(description = "The unique name of the brand to delete", example = "Ibanez") @PathVariable String brandName) {
+        boolean isDeleted = iBrandService.deleteBrand(brandName);
+        if (isDeleted) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(GuitarStoreConstants.STATUS_200,
+                    "Brand and associated guitars deleted successfully"));
+        } else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(GuitarStoreConstants.STATUS_417, "Delete operation failed"));
+        }
+    }
 }
